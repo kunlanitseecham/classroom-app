ce8dd3d07d617a23603907558c9fd9e6f74bcae4
kunlanitseecham <kunlanit.soda@gmail.com>
2026-05-09 13:22:27 +0000

    auto create tables

--- app.py
+++ app.py
@@ -394,7 +394,9 @@ def grade(id):
     return redirect("/teacher")
 
 # ================= MAIN =================
-
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
